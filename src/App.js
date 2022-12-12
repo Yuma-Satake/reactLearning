a86d5bb5199efcf423d29f1e750314834963e371
Yuma Satake <satous.y@icloud.com>
2022-12-12 23:43:49 +0900

    12/12

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline'
 
-// import HomePage from "./Components/HomeConponents/HomePage.js";
-// import EventPage from "./Components/EventConponents/EventPage.js";
+import HomePage from "./Components/HomeConponents/HomePage.js";
+import EventPage from "./Components/EventConponents/EventPage.js";
 import TeamSetPage from "./Components/TeamSetConponents/TeamSetPage.js"; 
 
 function App() {
   return (
     <>
       <CssBaseline></CssBaseline>
-      {/* <HomePage></HomePage> */}
-      {/* <EventPage></EventPage> */}
+      <HomePage></HomePage>
+      <EventPage></EventPage>
       <TeamSetPage></TeamSetPage>
     </>
   );
