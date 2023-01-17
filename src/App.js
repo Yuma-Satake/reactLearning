79556809c41c31db83ddc78859dcb84b7dae8c9c
Yuma-Satake <satous.y@icloud.com>
2023-01-17 16:48:57 +0900

    ページ削除

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline'
 
 import HomePage from "./Components/HomeConponents/HomePage.js";
-import EventPage from "./Components/EventConponents/EventPage.js";
-import TeamSetPage from "./Components/TeamSetConponents/TeamSetPage.js"; 
 
 function App() {
   return (
     <>
       <CssBaseline></CssBaseline>
       <HomePage></HomePage>
-      <EventPage></EventPage>
-      <TeamSetPage></TeamSetPage>
     </>
   );
 };
