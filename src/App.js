ab8b7d28b8d2160406689c0b4e8edf69fdb93f53
Yuma Satake <satous.y@icloud.com>
2023-02-27 23:34:35 +0900

    ファイル整理

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Stack } from '@mui/material';
-import MailSend from './Components/mail';
-
-const sub = () => {
-}
+import { Stack, Typography } from '@mui/material';
+import MailSend2 from './Components/MailSend2';
+import MailSend from './Components/MailSend';
 
 const App = () => {
   return (
-    <Stack justifyContent='center' sx={{m:50}}>
+    <Stack justifyContent='center' spacing={2} sx={{m:50}}>
+      <Typography>send</Typography>
       <MailSend/>
+      <Typography>send2</Typography>
+      <MailSend2/>
     </Stack>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getAuth} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-
-
