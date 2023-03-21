d2fa15d3bbc50c32f503694737a89a09da11d299
Yuma Satake <satous.y@icloud.com>
2023-03-21 23:01:39 +0900

    D＆D試した

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { Stack, Typography } from '@mui/material';
-import MailSend2 from './Components/MailSend2';
-import MailSend from './Components/MailSend';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { ListItem, Stack, Table, TableBody, TableCell, TableRow, Typography } from "@mui/material";
+import { useState } from "react";
+import { Drags } from "./Components/Drags.tsx";
 
 const App = () => {
   return (
-    <Stack justifyContent='center' spacing={2} sx={{m:50}}>
-      <Typography>send</Typography>
-      <MailSend/>
-      <Typography>send2</Typography>
-      <MailSend2/>
+    <Stack justifyContent='center' spacing={2} sx={{ m: 10 }}>
+      <Drags />
     </Stack>
   );
 };
 
 export default App;
-
