1f51c59a1adc2dec8fa03a2ba3d6c10a91a22834
Yuma Satake <satous.y@icloud.com>
2023-03-29 16:24:20 +0900

    Reinit

--- src/Components/SortList.ts
+++
@@ -1,4 +0,0 @@
-export const SortList = ({ List, setList, DragItem }) => {
-  // const newList = List.splice(DragItem.drop, 1, { select: 1000, drop: 10000 });
-  setList([...List, { id: 1000, num: 10000 }]);
-};
