176d719d1bc2adc662aa0b53773f8b1acf1bb29f
Yuma Satake <satous.y@icloud.com>
2023-04-22 23:41:11 +0900

    overwatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,45 @@
 import { Button, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import axios, { AxiosResponse } from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QueryClient, useQuery } from "react-query";
 
 export function App() {
-  const [disp, setdisp] = useState({ id: 0, name: "未取得" });
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false
-      }
-    }
-  });
+  // const [disp, setdisp] = useState({ id: 0, name: "未取得" });
+  // const queryClient = new QueryClient({
+  //   defaultOptions: {
+  //     queries: {
+  //       refetchOnWindowFocus: false
+  //     }
+  //   }
+  // });
 
-  const eventHandle = async () => {
-    const res: AxiosResponse = await axios.get("http://localhost:5173/name");
-    setdisp(res.data);
+  // const eventHandle = async () => {
+  //   const res: AxiosResponse = await axios.get("http://localhost:5173/name");
+  //   setdisp(res.data);
+  // };
+
+  // const useQueryFn = useQuery("name", eventHandle);
+
+  const [value, setValue] = useState("");
+
+  const handle = async () => {
+    const res = await axios.get("https://best-overwatch-api.herokuapp.com/");
+    console.log(res);
+    setValue(res.data);
   };
-
-  const useQueryFn = useQuery("name", eventHandle);
 
   return (
     <Stack direction='column' spacing={5} sx={{ m: 10 }}>
-      <Button variant='contained' onClick={eventHandle}>
-        取得ボタン
+      <Typography>{value}</Typography>
+      <Button
+        variant='contained'
+        onClick={() => {
+          handle();
+        }}
+      >
+        ヒーロー
       </Button>
-      <Typography>{`IDは：${disp.id}`}</Typography>
-      <Typography>{`名前は：${disp.name}`}</Typography>
     </Stack>
   );
 }
