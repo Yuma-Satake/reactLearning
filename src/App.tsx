18a46f2ca11310bc7f52ef705136460a76737a5b
Yuma Satake <satous.y@icloud.com>
2023-04-25 22:45:15 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,6 @@ import { useEffect, useState } from "react";
 import { QueryClient, useQuery } from "react-query";
 
 export function App() {
-  // const [disp, setdisp] = useState({ id: 0, name: "未取得" });
-  // const queryClient = new QueryClient({
-  //   defaultOptions: {
-  //     queries: {
-  //       refetchOnWindowFocus: false
-  //     }
-  //   }
-  // });
-
-  // const eventHandle = async () => {
-  //   const res: AxiosResponse = await axios.get("http://localhost:5173/name");
-  //   setdisp(res.data);
-  // };
-
-  // const useQueryFn = useQuery("name", eventHandle);
-
-  const [value, setValue] = useState("");
-
-  const handle = async () => {
-    const res = await axios.get("https://best-overwatch-api.herokuapp.com/");
-    console.log(res);
-    setValue(res.data);
-  };
-
   return (
     <Stack direction='column' spacing={5} sx={{ m: 10 }}>
       <Typography>{value}</Typography>
